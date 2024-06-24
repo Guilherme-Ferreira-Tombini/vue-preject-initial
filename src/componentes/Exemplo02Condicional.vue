641f1a9a74178b5f1b10e17573472b27ac89b459
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-06-24 20:22:46 -0300

    Utilizando v-if, v-else-if e v-else

--- src/componentes/Exemplo02Condicional.vue
+++ src/componentes/Exemplo02Condicional.vue
@@ -1,9 +1,15 @@
 <!-- SCRIPT -->
 <script setup>
     let exibir = true;
+    let media = 10;
 </script>
 
 <!-- HTML -->
  <template>
-    <h1 v-if="exibir">Hello World!</h1>
+    <h1 v-if="exibir">Olá usuário</h1>
+
+    <!-- Utilizando comandos de "if" e "else" para comparações -->
+    <p v-if="media >= 7">Aprovado(a)</p>
+    <p v-else-if="media < 7 && media >= 3">Em exame</p>
+    <p v-else>Reprovado(a)</p>
  </template>
