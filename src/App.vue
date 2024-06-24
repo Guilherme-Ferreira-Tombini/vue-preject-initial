6da0f61c523fd38b1fcf27983ddc128391374893
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-06-24 19:13:30 -0300

    Trabalhando com componentes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <!-- SCRIPT -->
-<script setup>
-  let nome = 'Alice';
-  let idade = 25;
-  let numero1 = 10;
-  let numero2 = 5;
-</script>
+ <script setup>
+  //Importação dos componentes
+  import Exemplo01 from './componentes/Exemplo01Interpolacao.vue';
+ </script>
 
 <!-- HTML -->
  <template>
-  <p>{{'Olá '+nome+", sua idade é "+idade}}</p>
-  <p>{{ numero1+numero2 }}</p>
+    <Exemplo01/>
  </template>
---
+++ src/componentes/Exemplo01Interpolacao.vue
@@ -0,0 +1,13 @@
+<!-- SCRIPT -->
+<script setup>
+  let nome = 'Rebeca';
+  let idade = 32;
+  let numero1 = 20;
+  let numero2 = 10;
+</script>
+
+<!-- HTML -->
+ <template>
+  <p>{{'Olá '+nome+", sua idade é "+idade}}</p>
+  <p>{{ numero1+numero2 }}</p>
+ </template>
