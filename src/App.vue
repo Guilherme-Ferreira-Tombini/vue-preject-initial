5e0e3e3c050313617a2f03cf97500214f5c094af
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-06-24 20:45:26 -0300

    Utilizando a diretiva v-html

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
   //Importação dos componentes
   import Exemplo01 from './componentes/Exemplo01Interpolacao.vue';
   import Exemplo02 from './componentes/Exemplo02Condicional.vue';
-  import Exemplo03 from './componentes/Exemplo03Laco.vue'
+  import Exemplo03 from './componentes/Exemplo03Laco.vue';
+  import Exemplo04 from './componentes/Exemplo04HTML.vue';
  </script>
 
 <!-- HTML -->
@@ -11,4 +12,5 @@
     <Exemplo01/>
     <Exemplo02/>
     <Exemplo03/>
+    <Exemplo04/>
  </template>
---
+++ src/componentes/Exemplo04HTML.vue
@@ -0,0 +1,13 @@
+<!-- SCRIPT -->
+<script setup>
+    let texto = '<h1>Aprendendo a diretiva v-html!</h1>';
+</script>
+
+<!-- HTML -->
+ <template>
+    <!-- Foco em estruturas html, no caso uma estrutura que tera o texto -->
+    <div v-html="texto"></div> 
+    
+    <!-- No caso a interpolação de texto não converte em uma estrutura HTML -->
+    <div>{{ texto }}</div>
+ </template>
