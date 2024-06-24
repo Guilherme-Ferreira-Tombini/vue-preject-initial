4ee2c2e37f6064995ed03d42b0f10ffb0cc3b0dd
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-06-24 20:36:15 -0300

    Utilizando v-for

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
   //Importação dos componentes
   import Exemplo01 from './componentes/Exemplo01Interpolacao.vue';
   import Exemplo02 from './componentes/Exemplo02Condicional.vue';
+  import Exemplo03 from './componentes/Exemplo03Laco.vue'
  </script>
 
 <!-- HTML -->
  <template>
     <Exemplo01/>
     <Exemplo02/>
+    <Exemplo03/>
  </template>
---
+++ src/componentes/Exemplo03Laco.vue
@@ -0,0 +1,12 @@
+<!-- SCRIPT -->
+<script setup>
+    let nomes = ['Alice', 'Bruna', 'Caio', 'Daniel', 'Ellen'];
+</script>
+
+<!-- HTML -->
+ <template>
+    <ul>
+        <!-- Utilização de um v-for para apresentar um vetor de nomes -->
+        <li v-for="i in nomes">{{ i }}</li>
+    </ul>
+ </template>
