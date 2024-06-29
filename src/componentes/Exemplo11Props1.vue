30b0fb0c9310a449a089d253ae16813634ac87fd
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-06-29 16:36:18 -0300

    Trabalhando com props dinamicos

--- src/componentes/Exemplo11Props1.vue
+++ src/componentes/Exemplo11Props1.vue
@@ -1,9 +1,17 @@
 <!-- SCRIPT -->
 <script setup>
     import Exemplo11Props2 from './Exemplo11Props2.vue';
+
+    // VARIAVEL QUE PASSARA DE FORMA DINAMICA
+    let cidade = "São Paulo";
+    let cidades =["Curitiba", "Florianopolis", "Rio de Janeiro"];
+
+    function mensagem(){
+        alert("Função criada para exemplo de props")
+    }
 </script>
 
 <!-- HTML -->
 <template>
-    <Exemplo11Props2 nome="Ralf" idade="33"/>
+    <Exemplo11Props2 nome="Ralf" idade="33" :cidade="cidade" :cidades="cidades" :funcao="mensagem"/>
 </template>
--- src/componentes/Exemplo11Props2.vue
+++ src/componentes/Exemplo11Props2.vue
@@ -1,10 +1,17 @@
 <!-- SCRIPT -->
 <script setup>
-    const props = defineProps(['nome', 'idade']);
+    const props = defineProps(['nome', 'idade', 'cidade', 'cidades', 'funcao']);
 </script>
 
 <!-- HTML -->
 <template>
     <h1>{{ nome }}</h1>
     <h1>{{ idade }}</h1>
+    <h1>{{ cidade }}</h1>
+
+    <ul>
+        <li v-for="v of cidades">{{ v }}</li>
+    </ul>
+
+    <button @click="funcao">Clique aqui</button>
 </template>
